8bbb8211cc4875738d495647ea2332094ad6922d
BounharAbdelaziz <bounhar.abdelaziz@gmail.com>
2025-02-03 09:12:00 +0100

    added helper functions

--- utils.py
+++ utils.py
@@ -1,23 +1,22 @@
 import gc
 from datasets import Dataset
+import os
+import glob
+
     
 # ------------------------------------------------------------------------------------------------------------ #
 # ------------------------------------------------------------------------------------------------------------ #
 
 def clean_dataset(dataset, target_lang):
-
-    # transform to pandas dataframe
-    df = dataset.to_pandas()
-    
-    # Safety check: Remove empty rows
+    # Safety check: Remove empty rows where the target_lang column is NaN
     print(f'[INFO] Filter out empty examples...')
-    df = df.dropna(subset=[target_lang])
-    dataset = Dataset.from_pandas(df)
+    
+    # Directly filter the dataset
+    dataset = dataset.filter(lambda example: example[target_lang] is not None and example[target_lang] != '')
+    
     print(f'[INFO] Filtering ended.')
-    
-    dataset = Dataset.from_pandas(df)
     return dataset
-    
+
 # ------------------------------------------------------------------------------------------------------------ #
 # ------------------------------------------------------------------------------------------------------------ #
 
@@ -40,5 +39,35 @@ def get_save_dir_path(HUB, MODEL_NAME, TRAIN_ON_ALL_DATA, TRAIN_ON_ARABIZI_ONLY,
 
     return save_model_path
     
+# ------------------------------------------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------------------------------------------ #
+
+def find_checkpoint_path(base_path):
+    
+    """ Search for the checkpoint file in the directory """
+    # Use glob to search for the checkpoint file
+    checkpoint_pattern = os.path.join(base_path, "checkpoint-*")
+    checkpoints = glob.glob(checkpoint_pattern)
+    
+    if checkpoints:
+        # Assuming there's only one checkpoint file
+        return checkpoints[0]
+    else:
+        raise FileNotFoundError("No checkpoint file found in the directory.")
+    
+# ------------------------------------------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------------------------------------------ #
+
+def preprocess_function(examples, tokenizer, max_seq_length=1024, text_field="text"):
+    tokenized = tokenizer(
+        examples[text_field],
+        padding="max_length",
+        truncation=True,
+        max_length=max_seq_length,
+        return_tensors=None, # returns a list #"pt"
+    )
+    tokenized["labels"] = tokenized["input_ids"][:]
+    return tokenized
+
 # ------------------------------------------------------------------------------------------------------------ #
 # ------------------------------------------------------------------------------------------------------------ #
